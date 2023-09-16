9fb523950b905e07c6e32fc8bba26822f1f1d9af
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2023-09-16 13:55:43 +0530

    added mobile menu toggle

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ AOS.init({
 	duration: 1000,
     offset: 100,
 });
+
+
+// Get references to the mobile menu and toggle button
+const mobileMenu = document.querySelector(".header_menu");
+const mobileMenuToggle = document.querySelector(".header_menu-mobile");
+
+// Add a click event listener to the toggle button
+mobileMenuToggle.addEventListener("click", function () {
+
+	const visibility = mobileMenu.getAttribute('data-visible');
+
+	if(visibility === 'false') {
+        mobileMenu.setAttribute('data-visible', true);
+        mobileMenuToggle.setAttribute('aria-expanded', true);
+    } else {
+        mobileMenu.setAttribute('data-visible', false);
+        mobileMenuToggle.setAttribute('aria-expanded', false);
+    }
+});
